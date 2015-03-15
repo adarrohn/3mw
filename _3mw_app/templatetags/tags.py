62cfb18bdce9ea8dbd706bc0d884b41ce3ed4fa0
Andrew <andrew@adarrohn.com>
2015-03-16 00:47:36 +0100

    cleaned up template debugging button active

--- _3mw_app/templatetags/tags.py
+++ _3mw_app/templatetags/tags.py
@@ -5,6 +5,7 @@ register = template.Library()
 @register.simple_tag
 def active(request, pattern):
     import re
+    print(pattern, request.path)
     if re.match(pattern, request.path):
         return 'active'
     else:
