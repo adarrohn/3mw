9d2e2652fe780dd19b4b4c0b682c4fe2afd35f10
Andrew <andrew@adarrohn.com>
2015-03-15 23:47:27 +0100

    removed now necessary mixin object added summary base template sum and avg summary implemented with django or

--- _3mw_app/views.py
+++ _3mw_app/views.py
@@ -1,19 +1,15 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.db.models import Sum, Avg
+
 from .models import Site, Entry
 
 
-class SiteMixin(object):
-    """
-    Mixin for all Site Model Views
-    """
-    model = Site
-    context_object_name = 'sites'
-
-
-class SitesList(SiteMixin, ListView):
+class SitesList(ListView):
     """
     Shows All Sites as a List View
     """
+    model = Site
+    context_object_name = 'sites'
     template_name = 'sites_list.html'
 
 
@@ -21,22 +17,32 @@ class SitesDetail(ListView):
     """
     Shows a Single Site in a List View Containing Foreign Entries
     """
-    template_name = 'sites_detail.html'
     context_object_name = 'entries'
+    template_name = 'sites_detail.html'
 
     def get_queryset(self):
         return Entry.objects.filter(site=self.kwargs['id'])
 
 
-class SummaryListSum(SiteMixin, ListView):
+class SummaryListSum(ListView):
     """
-
+    Shows an Aggregated Sum of A And B Values From Each Site
     """
-    template_name = 'summary_list.html'
+    template_name = 'summary_sum_list.html'
+    context_object_name = 'sites'
+
+    def get_queryset(self):
+        return Site.objects.all().annotate(sum_val_a=Sum('entry__val_a'),
+                                           sum_val_b=Sum('entry__val_b'))
 
 
-class SummaryListAverage(SiteMixin, ListView):
+class SummaryListAverage(ListView):
     """
-
+    Shows an Average of A and B Values From Each Site
     """
     template_name = 'summary_avg_list.html'
+    context_object_name = 'sites'
+
+    def get_queryset(self):
+        return Site.objects.all().annotate(avg_val_a=Avg('entry__val_a'),
+                                           avg_val_b=Avg('entry__val_b'))
