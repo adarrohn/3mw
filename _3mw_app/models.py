56a00e776c862adfea122d989eb61f517737fd39
Andrew <andrew@adarrohn.com>
2015-03-15 16:01:40 +0100

    added site model

--- _3mw_app/models.py
+++ _3mw_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Site(models.Model):
+    """
+    Model representation for a Site
+    """
+    name = models.CharField(max_length=50)
+    date = models.DateField()
+    val_a = models.DecimalField()
+    val_b = models.DecimalField()
+
+    class Meta:
+        verbose_name = 'Site'
+        verbose_name_plural = 'Sites'
+
+    def __str__(self):
+        return 'Site {}'.format(self.name)
