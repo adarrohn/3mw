d5a60747d59a6fbee5125f093e6b2a6b114e9c3d
Andrew <andrew@adarrohn.com>
2015-03-15 16:43:51 +0100

    added tempaltes to cbvs added pk capture to detail regex

--- _3mw/urls.py
+++ _3mw/urls.py
@@ -6,26 +6,26 @@ urlpatterns = patterns('',
                        # List View for Sites Page
                        # matches /, /sites/, /sites
                        url(r'(^sites/?$)|(^/?$)',
-                           SitesList.as_view(),
+                           SitesList.as_view(template_name='sites_list.html'),
                            name='sites'),
 
                        # Detail View for Sites
                        # matches /sites/#, /sites/#/
-                       url(r'^sites/\d+}/?$',
-                           SitesDetail.as_view(),
+                       url(r'^sites/(?P<pk>\d+)/?$',
+                           SitesDetail.as_view(template_name='sites_detail.html'),
                            name='sites_detail'),
 
                        # List View for Summary Page
                        # matches /summary, /summary/
                        url(r'^summary/?$',
-                           SummaryList.as_view(),
+                           SummaryList.as_view(template_name='summary_list.html'),
                            name='summary'),
 
                        # List View for Summary Average Page
                        # matches /summary-average, /summary-average/
                        url(r'^summary-average/?$',
-                           SummaryListAverage.as_view(),
-                           name='summary_average'),
+                           SummaryListAverage.as_view(template_name='summary_avg_list.html'),
+                           name='summary_avg'),
 
                        # Admin
                        url(r'^admin/',
