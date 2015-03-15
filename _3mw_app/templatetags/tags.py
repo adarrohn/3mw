c34177b7181335e62eeca4fb7b256c4ec125ae90
Andrew <andrew@adarrohn.com>
2015-03-16 00:27:06 +0100

    swapped re.search for re.match in active tag

--- _3mw_app/templatetags/tags.py
+++ _3mw_app/templatetags/tags.py
@@ -5,7 +5,7 @@ register = template.Library()
 @register.simple_tag
 def active(request, pattern):
     import re
-    if re.search(pattern, request.path):
+    if re.match(pattern, request.path):
         return 'active'
     else:
         return ''
