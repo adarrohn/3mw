6662a7b0f3bb104a6ceceda2f885f5e9cdb931a0
Andrew <andrew@adarrohn.com>
2015-03-16 01:18:11 +0100

    split tag into 2 -> basic match & exact match

--- _3mw_app/templatetags/tags.py
+++ _3mw_app/templatetags/tags.py
@@ -3,10 +3,23 @@ from django import template
 register = template.Library()
 
 @register.simple_tag
-def active(request, pattern):
+def active_basic(request, pattern):
+    """
+    Basic match anywhere in path
+    """
     import re
-    print('pattern={}'.format(pattern), 'path={}'.format(request.path), re.match(pattern, request.path))
     if re.match(pattern, request.path):
+        return 'active'
+    else:
+        return ''
+
+@register.simple_tag
+def active_exact(request, pattern):
+    """
+    Only matches an exact path
+    """
+    import re
+    if re.match(r'^{}/?$'.format(pattern), request.path):
         return 'active'
     else:
         return ''
