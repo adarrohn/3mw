e0492f27977f1a375dceebb000e35f175badfa0d
Andrew <andrew@adarrohn.com>
2015-03-16 01:08:30 +0100

    added trailing / to button anchors

--- _3mw_app/templatetags/tags.py
+++ _3mw_app/templatetags/tags.py
@@ -5,7 +5,7 @@ register = template.Library()
 @register.simple_tag
 def active(request, pattern):
     import re
-    print(pattern, request.path)
+    print('pattern={}'.format(pattern), 'path={}'.format(request.path), re.match(pattern, request.path))
     if re.match(pattern, request.path):
         return 'active'
     else:
